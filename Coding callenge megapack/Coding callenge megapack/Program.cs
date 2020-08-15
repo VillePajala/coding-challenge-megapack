da8ea0034dac583b1f514c66e886d0801cb94285
VillePajala <valoraami@gmail.com>
2020-08-15 21:00:32 +0300

    Project base

---
+++ Coding callenge megapack/Coding callenge megapack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coding_callenge_megapack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
