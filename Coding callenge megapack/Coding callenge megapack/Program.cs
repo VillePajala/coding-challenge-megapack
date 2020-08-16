d8b291bfa46bd5e715122d36ad75beec4d5a23a0
VillePajala <valoraami@gmail.com>
2020-08-16 14:30:12 +0300

    Added initial console menu for choosing challenge

--- Coding callenge megapack/Coding callenge megapack/Program.cs
+++ Coding callenge megapack/Coding callenge megapack/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Coding_callenge_megapack
 {
+    public class ChallengeMenu
+    {
+        
+        public static void ChooseChallenge()
+        {
+            String challengeNumber;
+
+            Console.WriteLine("Welcome to Coding challenge megapack!");
+            Console.WriteLine("");
+            Console.WriteLine("Type the number of the challenge you want to try out");
+            Console.WriteLine("");
+            Console.WriteLine("Convert numbers to roman numerals: 1s");
+            challengeNumber = Console.ReadLine();
+
+            RunChallenge(challengeNumber);
+        }
+
+        public static void RunChallenge(string challengeSelected)
+        {
+            Console.WriteLine(challengeSelected);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Oh yeah, oh yeah!");
+            ChallengeMenu.ChooseChallenge();
         }
     }
 }
