b6d1d507cdeecb54f7fad7289e393d2110227f20
VillePajala <valoraami@gmail.com>
2020-08-18 23:40:59 +0300

    Refactored numbers to roman numerals code

--- Coding callenge megapack/ConvertNumbersToRomanNumerals/ConvertNumbersToRomanNumerals.cs
+++ Coding callenge megapack/ConvertNumbersToRomanNumerals/ConvertNumbersToRomanNumerals.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConvertNumbersToRomanNumerals
 {
@@ -23,42 +24,51 @@ namespace ConvertNumbersToRomanNumerals
                 Console.WriteLine("Something went wrong. Make sure to input a NUMBER");
                 askUserForNumberToConvert();
             }
-
         }
 
         public static void convertInput(int inputNumber)
         {
-            int thousandsCount = inputNumber / 1000;
-            inputNumber = inputNumber - (thousandsCount * 1000);
-            int fiveHundredsCount = inputNumber / 500;
-            inputNumber = inputNumber - (fiveHundredsCount * 500);
-            int hundredsCount = inputNumber / 100;
-            inputNumber = inputNumber - (hundredsCount * 100);
+            
+
             string romanNumeralOutput = "";
 
+            int thousandscount = inputNumber / 1000;
+            int fivehundredscount = (inputNumber % 1000) / 500;
+            int hundredscount = ((inputNumber % 1000) % 500) / 100;
+            int tensCount = (((inputNumber % 1000) % 500) % 100) / 10;
+            int fivesCount = ((((inputNumber % 1000) % 500) % 100) % 10) / 5;
+            int onesCount = ((((inputNumber % 1000) % 500) % 100) % 10) & 5;
 
-            int loopCount = 0;
-            while(loopCount < thousandsCount)
+            Dictionary<String, Int32> romanNumeralDict = new Dictionary<String, Int32>()
+            romanNumeralDict.Add("M", thousandscount); 
+            romanNumeralDict.Add("D", fivehundredscount);
+            romanNumeralDict.Add("C", hundredscount);
+            romanNumeralDict.Add("X", tensCount);
+            romanNumeralDict.Add("V", fivesCount);
+            if (onesCount == 4) romanNumeralDict.Add("IV", 1); 
+            else romanNumeralDict.Add("I", onesCount);
+
+
+
+            foreach (KeyValuePair<String, Int32> romanNumeralAndTimes in romanNumeralDict)
             {
-                romanNumeralOutput = romanNumeralOutput + "M";
-                loopCount = loopCount + 1;
+                romanNumeralOutput = romanNumeralOutput + addRomanNumeralString(romanNumeralAndTimes.Key, romanNumeralAndTimes.Value);
+                
             }
-            
-            loopCount = 0;
-            while (loopCount < fiveHundredsCount)
-            {
-                romanNumeralOutput = romanNumeralOutput + "D";
-                loopCount = loopCount + 1;
-            }
-            
-            loopCount = 0;
-            while (loopCount < hundredsCount)
-            {
-                romanNumeralOutput = romanNumeralOutput + "C";
-                loopCount = loopCount + 1;
-            }
-            
             Console.WriteLine("The result of the conversion: " + romanNumeralOutput);
+        }
+
+
+            public static string addRomanNumeralString(string romanNumeralCharacter, int timesTheCharacterwillBeAdded)
+        {
+            int loopCount = 0;
+            string romanNumeralString = "";
+            while(loopCount < timesTheCharacterwillBeAdded)
+            {
+                romanNumeralString = romanNumeralString + romanNumeralCharacter;
+                loopCount = loopCount + 1;
+            }
+            return romanNumeralString;
         }
     }
 }
